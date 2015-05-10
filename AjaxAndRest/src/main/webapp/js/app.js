db8d2da242fd6f16ae27f963b803d039ad331539
zezutom <zezulatomas@gmail.com>
2015-05-10 19:18:23 +0200

    Search form: default values, thumbnails: resolved 404 (ng-src vs src in the img tag).

--- AjaxAndRest/src/main/webapp/js/app.js
+++ AjaxAndRest/src/main/webapp/js/app.js
@@ -1,17 +1,26 @@
 var ComposerTributeApp = angular.module('ComposerTributeApp', []);
 
-ComposerTributeApp.controller('ComposerListController', function($scope, $http) {
+ComposerTributeApp.controller('ComposerListController', function($scope, $http) {  
+    $scope.sortByOptions = [
+        {name: 'Surname', value: 'lname'},
+        {name: 'Genre', value: 'genre'},
+        {name: 'Birthday', value: 'born'}
+    ];
+    
+    $scope.filterByGenreDefault = {name: 'All', value: ' '};
+    
     $http.get('resources/composers').success(function(data) {
         $scope.composers = data;
-    }).error(function() {
-        console.log('There was an error with the API call.')
-    });
+    });            
+    $http.get('resources/genres').success(function(data) {
+        $scope.genres = data;
+    });            
 });
 
 ComposerTributeApp.filter('capitalize', function() {
     // Credit: http://codepen.io/WinterJoey/pen/sfFaK
     return function(input, all) {
-      return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, 
+      return (!!input) ? input.replace('_', ' ').replace(/([^\W_]+[^\s-]*) */g, 
         function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
     };
 });
