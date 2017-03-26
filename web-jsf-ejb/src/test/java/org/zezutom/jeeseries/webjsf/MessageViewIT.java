861255e09bb9dc39b456fa3af4f76cbd350e6be9
zezutom <tomas.zezula@panaseer.com>
2017-03-26 22:37:06 +0100

    JPA persistence integration tests.

--- web-jsf-ejb/src/test/java/org/zezutom/jeeseries/webjsf/MessageViewIT.java
+++ web-jsf-ejb/src/test/java/org/zezutom/jeeseries/webjsf/MessageViewIT.java
@@ -9,42 +9,55 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.zezutom.jeeseries.webjsf.model.Message;
-import org.zezutom.jeeseries.webjsf.service.AbstractFacade;
 import org.zezutom.jeeseries.webjsf.service.MessageFacade;
 
 @RunWith(Arquillian.class)
 public class MessageViewIT {
     
     private static final String WEBAPP_SRC = "src/main/webapp";
-    
-    private static final String RESOURCES_SRC = "src/main/resources";
-    
+        
     @Drone
     private WebDriver browser;
     
     @ArquillianResource
     private URL deploymentURL;
     
+    @FindBy(css = "input[type=text]")
+    private WebElement textField;
+
+    @FindBy(css = "input[type=submit]")
+    private WebElement submitButton;
+    
+    @FindBy(id = "numberOfMessages")
+    private WebElement outputText;
+    
     // Runs in a client mode
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
         return ShrinkWrap
-                .create(WebArchive.class, "message.war")                
+                .create(WebArchive.class, "web-jsf-ejb.war")                
                 .addPackage(Message.class.getPackage())
                 .addPackage(MessageFacade.class.getPackage())
                 .addPackage(MessageView.class.getPackage())
                 .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
                 .addAsWebResource(new File(WEBAPP_SRC, "index.xhtml"))
-                .addAsWebResource(new File(WEBAPP_SRC, "result.xhtml"))                
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+                .addAsWebResource(new File(WEBAPP_SRC, "result.xhtml")) 
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .setWebXML(new File(WEBAPP_SRC + "/WEB-INF/web.xml"));
     }
     
     @Test
     public void sentMessageShouldCount() {
-        browser.get(deploymentURL + "index.xhtml");
+        browser.get(deploymentURL.toString());
+        textField.sendKeys("Test message");
+        submitButton.click();
+        Assert.assertEquals("1", outputText.getText());        
     }
 }
---
+++ web-jsf-ejb/src/test/java/org/zezutom/jeeseries/webjsf/service/MessageFacadeIT.java
@@ -0,0 +1,79 @@
+package org.zezutom.jeeseries.webjsf.service;
+
+import java.util.List;
+import javax.ejb.EJB;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.zezutom.jeeseries.webjsf.model.Message;
+
+@RunWith(Arquillian.class)
+public class MessageFacadeIT {
+    
+    @EJB
+    private MessageFacade messageFacade;
+    
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap
+                .create(JavaArchive.class, "web-jsf-ejb.jar")
+                .addPackage(MessageFacade.class.getPackage())
+                .addPackage(Message.class.getPackage())
+                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+    
+    @After
+    public void tearDown() {
+        List<Message> messages = messageFacade.findAll();
+        if (messages != null) {
+            messages.stream().forEach(message -> messageFacade.remove(message));
+        }
+        Assert.assertEquals(0, messageFacade.count());
+    }
+    
+    @Test
+    public void createdMessageHasIdAndCanBeFound() {
+        Message message = newMessage("test");
+        messageFacade.create(message);
+        Assert.assertNotNull(message.getId());
+        Assert.assertEquals(message, messageFacade.find(message.getId()));        
+    }
+    
+    @Test
+    public void createdMessageCanBeChanged() {
+        Message message = newMessage("test");
+        messageFacade.create(message);
+
+        String newText = "test 1";
+        message = messageFacade.find(message.getId());
+        message.setMessage(newText);
+        
+        messageFacade.edit(message);
+        Assert.assertEquals(message, messageFacade.find(message.getId()));        
+    }
+   
+    @Test
+    public void everyMessageCounts() {
+        Message[] messages = {
+            newMessage("one"), newMessage("two"), newMessage("three")
+        };
+        for (Message message : messages) {
+            messageFacade.create(message);
+        }
+        Assert.assertEquals(messages.length, messageFacade.count());
+    }
+    
+    private Message newMessage(String text) {
+        Message message = new Message();
+        message.setMessage(text);
+        Assert.assertNull(message.getId());
+        return message;
+    }
+}
