c11dce60fdc533340af15b52c2c2860ce4388f26
zezutom <zezulatomas@gmail.com>
2017-04-08 19:52:39 +0100

    DAO integration tests.

--- web-jsf-ajax-rest/src/main/java/org/zezutom/web/jsf/ajax/rest/service/AbstractFacade.java
+++ web-jsf-ajax-rest/src/main/java/org/zezutom/web/jsf/ajax/rest/service/AbstractFacade.java
@@ -1,7 +1,13 @@
 package org.zezutom.web.jsf.ajax.rest.service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Root;
 
 public abstract class AbstractFacade<T> {
 
@@ -29,15 +35,21 @@ public abstract class AbstractFacade<T> {
         return getEntityManager().find(entityClass, id);
     }
 
-    public List<T> findAll() {
-        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        cq.select(cq.from(entityClass));
+    public List<T> findAll(String... orderBy) {
+        final CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+        javax.persistence.criteria.CriteriaQuery cq = criteriaBuilder.createQuery();
+        final Root from = cq.from(entityClass);
+        cq.select(from);
+        orderBy(orderBy, criteriaBuilder, from, cq);
         return getEntityManager().createQuery(cq).getResultList();
     }
 
-    public List<T> findRange(int[] range) {
-        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        cq.select(cq.from(entityClass));
+    public List<T> findRange(int[] range, String... orderBy) {
+        final CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+        javax.persistence.criteria.CriteriaQuery cq = criteriaBuilder.createQuery();
+        final Root from = cq.from(entityClass);
+        cq.select(from);
+        orderBy(orderBy, criteriaBuilder, from, cq);
         javax.persistence.Query q = getEntityManager().createQuery(cq);
         q.setMaxResults(range[1] - range[0] + 1);
         q.setFirstResult(range[0]);
@@ -51,5 +63,13 @@ public abstract class AbstractFacade<T> {
         javax.persistence.Query q = getEntityManager().createQuery(cq);
         return ((Long) q.getSingleResult()).intValue();
     }
-    
+
+    private void orderBy(String[] orderBy, final CriteriaBuilder criteriaBuilder, final Root from, CriteriaQuery cq) {
+        if (orderBy != null && orderBy.length > 0) {
+            final List<Order> orderByExprs = Arrays.asList(orderBy)
+                    .stream().map(field -> criteriaBuilder.asc(from.get(field)))
+                    .collect(Collectors.toList());
+            cq.orderBy(orderByExprs);
+        }
+    }
 }
--- web-jsf-ajax-rest/src/test/java/org/zezutom/web/jsf/ajax/rest/service/ComposerFacadeIT.java
+++ web-jsf-ajax-rest/src/test/java/org/zezutom/web/jsf/ajax/rest/service/ComposerFacadeIT.java
@@ -1,5 +1,6 @@
 package org.zezutom.web.jsf.ajax.rest.service;
 
+import java.util.List;
 import javax.ejb.EJB;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -10,6 +11,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.zezutom.web.jsf.ajax.rest.model.Composer;
+import org.zezutom.web.jsf.ajax.rest.model.Composition;
 
 @RunWith(Arquillian.class)
 public class ComposerFacadeIT {
@@ -20,16 +22,53 @@ public class ComposerFacadeIT {
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap
-                .create(JavaArchive.class, "web-jsf-ajax-rest.jar")
+                .create(JavaArchive.class, "test.jar")
                 .addPackage(ComposerFacade.class.getPackage())
                 .addPackage(Composer.class.getPackage())
                 .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-                .addAsResource("test-data.sql", "META-INF/data.sql")
+                .addAsResource("test-data.sql", "META-INF/test-data.sql")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }    
     
     @Test
     public void shouldCreateDbAndLoadData() {
-        Assert.assertTrue(composerFacade.count() == 1);
+        Assert.assertTrue(composerFacade.count() == 10);
+        composerFacade.findAll().stream().forEach(composer -> {
+            List<Composition> compositions = composer.getCompositions();
+            Assert.assertNotNull(compositions);
+            Assert.assertFalse(compositions.isEmpty());
+        });
+    }
+    
+    @Test
+    public void shouldServePageOfResults() {
+        List<Composer> firstPage = composerFacade.findRange(new int[] {0, 3});
+        List<Composer> secondPage = composerFacade.findRange(new int[] {4, 7});
+        List<Composer> thirdPage = composerFacade.findRange(new int[] {8, 11});
+
+        assertPageOfResults(firstPage, 4);
+        assertPageOfResults(secondPage, 4);
+        assertPageOfResults(thirdPage, 2);               
+    }
+    
+    @Test
+    public void shouldOrderBySpecifiedFields() {
+        // Top 3 by genre and year of birth
+        List<Composer> composers = composerFacade.findAll("genre", "born").subList(0, 3);
+        assertComposer(composers.get(0), "Baroque", 1653, "Corelli");
+        assertComposer(composers.get(1), "Baroque", 1659, "Purcell");
+        assertComposer(composers.get(2), "Baroque", 1678, "Vivaldi");
+    }
+    
+    private void assertComposer(Composer composer, String genre, int born, String lastName) {
+        Assert.assertNotNull(composer);        
+        Assert.assertTrue(composer.getBorn() == born);
+        Assert.assertEquals(genre, composer.getGenre());
+        Assert.assertEquals(lastName, composer.getLastName());
+    }
+    
+    private void assertPageOfResults(List<Composer> pageOfResults, int expectedSize) {
+        Assert.assertNotNull(pageOfResults);
+        Assert.assertTrue(pageOfResults.size() == expectedSize);
     }
 }
