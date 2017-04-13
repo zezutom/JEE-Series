c3966101782fc9dc15b6bdcf7c3b23770bbf6e13
zezutom <zezulatomas@gmail.com>
2017-04-13 10:17:50 +0300

    Wrapping it up on pagination, filtering and sorting by multiple fields.

--- web-jsf-ajax-rest/src/main/webapp/js/app.js
+++ web-jsf-ajax-rest/src/main/webapp/js/app.js
@@ -1,28 +1,36 @@
-var ComposerTributeApp = angular.module('ComposerTributeApp', ['infinite-scroll', 'ui.bootstrap']);
+var ComposerTributeApp = angular.module('ComposerTributeApp', ['infinite-scroll']);
 
 ComposerTributeApp.controller('ComposerListController', function ($scope, $http) {
-    $scope.sortByOptions = [
-        {name: 'Surname', value: 'lastName'},
-        {name: 'Genre', value: 'genre'},
-        {name: 'Birthday', value: 'born'}
-    ];
 
-    var init = function () {
+    $scope.composers = [];
+    $scope.page = 0;
+    $scope.busy = false;
+    $scope.fullyLoaded = false;
+
+    $scope.sortOptions = [
+        {name: 'Name A-Z',  expr: 'lastName:asc'},
+        {name: 'Name Z-A',  expr: 'lastName:desc'},
+        {name: 'Genre A-Z', expr: 'genre:asc,lastName:asc'},        
+        {name: 'Genre Z-A', expr: 'genre:desc,lastName:asc'}        
+    ];
+    $scope.selectedSort = $scope.sortOptions[0];
+    
+    $scope.selectSort = function() {
         $scope.composers = [];
-        $scope.selectedComposer = undefined;
         $scope.page = 0;
         $scope.busy = false;
         $scope.fullyLoaded = false;
+        $scope.nextPage();
     };
-
-
+    
     $scope.nextPage = function () {
         if ($scope.busy || $scope.fullyLoaded)
             return;
         $scope.busy = true;
         $http.get('resources/composers/list', {
             params: {
-                page: $scope.page
+                page: $scope.page,
+                sortBy: $scope.selectedSort.expr
             }
         }).then(function (response) {
             var data = response.data;
@@ -37,45 +45,35 @@ ComposerTributeApp.controller('ComposerListController', function ($scope, $http)
     };
 
     $scope.filterComposers = function (val) {
-        var filtered = $scope.composers.filter(function(composer) {
-            return composer.lastName.toLowerCase().indexOf(val.trim().toLowerCase()) >= 0;
-        });
-        if (filtered.length > 0) return filtered;
-        
-        $scope.busy = true;
-        return $http.get('resources/composers/filter', {
-            params: {
-                query: val
-            }
-        }).then(function (response) {
-            return response.data.map(function (item) {
-                return item;
+        var normalize = function (str) {
+            return (str === undefined) ? "" : str.toLowerCase().trim();
+        };
+        var filter = function (composer) {
+            return normalize(composer.firstName + composer.lastName).indexOf(normalize(val)) >= 0;
+        };
+        var composerExists = $scope.composers.filter(filter).length > 0;
+        if (val === undefined || composerExists || $scope.busy || $scope.fullyLoaded) {
+            return filter;
+        } else {
+            $scope.busy = true;
+            return $http.get('resources/composers/filter', {
+                params: {
+                    query: val
+                }
+            }).then(function (response) {
+                $scope.busy = false;
+                var data = response.data;
+                if (data && data.length > 0) {
+                    $scope.composers.push.apply($scope.composers, data);                
+                } else {
+                    $scope.fullyLoaded = true;
+                }
+                return response.data.map(function (item) {
+                    return item;
+                });                
             });
-            $scope.busy = false;
-        });
+        }
     };
-
-    $scope.onSelect = function ($item, $model, $label) {
-//        if ($scope.busy) return;
-
-        $scope.busy = true;
-        $scope.composers = [];
-        $http.get('resources/composers/' + $item.id).then(function (response) {
-            var data = response.data;
-            if (data && data.length > 0) {
-                $scope.composers.push.apply($scope.composers, data);
-            }
-            $scope.busy = false;
-        });
-    };
-    
-    $scope.clearFilter = function() {
-        init();
-        $scope.nextPage();
-    };
-    
-    // Initialise the model
-    init();
 
     // Load the first page
     $scope.nextPage();
