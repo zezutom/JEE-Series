d238977ca8913426280d668dad268249f9b64c8c
zezutom <zezulatomas@gmail.com>
2017-04-25 07:07:16 +0100

    Barebones of a single-page app via ng-route

---
+++ eshop/src/main/webapp/js/main.js
@@ -0,0 +1,31 @@
+var eShop = angular.module('eShop', ['ngRoute']);
+
+// Configure routing
+eShop.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'categoryController'
+        })
+        .when('/category', {
+            templateUrl: 'pages/category.html',
+            controller: 'categoryController'
+        });
+});
+
+eShop.controller('categoryController', function ($scope, $http) {
+
+    $scope.categories = [];
+
+    $scope.loadAll = function () {
+        $http.get('resources/categories').then(function (response) {
+            var data = response.data;
+            if (data && data.length > 0) {
+                $scope.categories.push.apply($scope.categories, data);
+            }
+        });
+    };
+
+    // Load all categories
+    $scope.loadAll();
+});
